92097f8155db6e52ceebcc82564aae7ef07acce3
Meilin (Linda) Li <106841467+limeilinda@users.noreply.github.com>
2022-08-13 17:34:24 -0400

    Spring boot initializr

---
+++ src/main/java/com/passion/stocktrax/StocktraxApplication.java
@@ -0,0 +1,13 @@
+package com.passion.stocktrax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktraxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktraxApplication.class, args);
+	}
+
+}
